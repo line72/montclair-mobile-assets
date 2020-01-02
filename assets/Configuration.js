a4eb764a0f3a5913fd17518a3b86da28db75cfee
Marcus Dillavou <line72@line72.net>
2020-01-01 17:39:34 -0700

    Initial version

---
+++ assets/Configuration.js
@@ -0,0 +1,31 @@
+/* -*- Mode: rjsx -*- */
+
+/*******************************************
+ * Copyright (2018)
+ *  Marcus Dillavou <line72@line72.net>
+ *  http://line72.net
+ *
+ * Montclair:
+ *  https://github.com/line72/montclair
+ *  https://montclair.line72.net
+ *
+ * Licensed Under the GPLv3
+ *******************************************/
+
+import AvailtecParser from './AvailtecParser';
+
+class Configuration {
+    constructor() {
+        // Mobile, AL
+        this.center = [30.6907508, -88.1413472];
+
+        this.agencies = [
+            {
+                name: 'Routes',
+                parser: new AvailtecParser('https://realtimemobile.availtec.com/infopoint')
+            }
+        ];
+    }
+}
+
+export default Configuration;
---
+++ config.py
@@ -0,0 +1,33 @@
+# -*- mode: python -*-
+
+import transmogrifier
+
+CONFIG = transmogrifier.Config(
+    build_dir = './build',
+    repo = 'mobile',
+    package_name = 'montclair-mobile',
+    name = 'Go Mobile',
+    description = 'Real Time Tracking of the Buses for Mobile, AL',
+    url = 'https://mobile.gotransitapp.com',
+    logo_svg = 'assets/logo.svg',
+    montclair_config = transmogrifier.MontclairConfig(
+        version = '1.2.2',
+        revision = 1,
+        title = 'Go Mobile',
+        first_run_text = "Welcome to Mobile, AL's Real Time Bus Tracker.<br /><br />Please select one or more routes to begin!",
+        configuration_js_file = 'assets/Configuration.js'
+    ),
+    ios_config = transmogrifier.MontclairiOSConfig(
+        version = '2.0.2',
+        revision = 1,
+        app_id = 'com.gotransitapp.mobile',
+        app_store_id = '',
+        app_store_url = 'REPLACE_ME'
+    ),
+    android_config = transmogrifier.MontclairAndroidConfig(
+        version = '1.0.2',
+        revision = 1,
+        app_id = 'com.gotransitapp.mobile',
+        play_store_url = 'https://play.google.com/store/apps/details?id=com.gotransitapp.mobile'
+    )
+)
